b038d088f3245ae0312b022765a05f877053d09a
dumb-meh <omorfaruk549@gmail.com>
2025-08-05 11:08:35 +0600

    minor changes

--- app/services/empowerment/empowerment_schema.py
+++ app/services/empowerment/empowerment_schema.py
@@ -1,8 +1,5 @@
 from pydantic import BaseModel
 from typing import Dict,List, Any
 
-class empowerment_request(BaseModel):
-    userinfo: Dict[str, Any]
-
 class empowerment_response(BaseModel):
     tasks: Dict[str, Any]
--- app/services/task_suggestion/task_suggestion_route.py
+++ app/services/task_suggestion/task_suggestion_route.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Body
 from .task_suggestion import TaskSuggestion
-from .task_suggestion_schema import task_suggestion_response
+from .task_suggestion_schema import task_suggestion_request,task_suggestion_response
 
 router= APIRouter()
 summary= TaskSuggestion()
@@ -8,7 +8,7 @@ summary= TaskSuggestion()
 @router.post("/suggestion",response_model=task_suggestion_response)
 async def get_summary():
     try:
-        response=summary.get_suggestion(request_data)
+        response=summary.get_suggestion(request=task_suggestion_request)
         return task_suggestion_response (response=response)
     
     except Exception as e:
